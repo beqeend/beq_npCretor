36bbc22dab320909cb40a0e1750847adc126129f
Batuhan <rissedevelopment@gmail.com>
2025-07-25 12:36:44 +0300

    Add files via upload

--- beq_npcCretor/config.lua
+++ beq_npcCretor/config.lua
@@ -7,13 +7,13 @@ Config.NPCs = {
         model = "a_m_m_business_01",
         coords = vector4(-267.0, -960.0, 31.2, 200.0),
         scenario = "WORLD_HUMAN_STAND_IMPATIENT",
-        event = "npccreator:openShop"
+        event = "beqeend1"
     },
     {
         model = "a_m_y_beach_01",
         coords = vector4(-1204.0, -1560.0, 4.6, 120.0),
         scenario = "WORLD_HUMAN_SMOKING",
-        event = "npccreator:startQuest"
+        event = "beqeend2"
     }
     -- İstediğin kadar ekleyebilirsin
 } 
--- beq_npcCretor/server.lua
+++ beq_npcCretor/server.lua
@@ -1,9 +1,9 @@
-RegisterNetEvent("npccreator:openShop")
-AddEventHandler("npccreator:openShop", function()
+RegisterNetEvent("beqeend1")
+AddEventHandler("beqeend1", function()
     TriggerClientEvent("chat:addMessage", source, { args = { "NPC", "Shop açıldı!" } })
 end)
 
-RegisterNetEvent("npccreator:startQuest")
-AddEventHandler("npccreator:startQuest", function()
+RegisterNetEvent("beqeend2")
+AddEventHandler("beqeend2", function()
     TriggerClientEvent("chat:addMessage", source, { args = { "NPC", "Görev başladı!" } })
 end) 
